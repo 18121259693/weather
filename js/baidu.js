50d27f84b7fc7d45bc2171084aaa86df43ced7b2
张明辉 <361186482@qq.com>
2018-06-08 08:02:54 +0800

    666

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "https://hm.baidu.com/hm.js?38867d98c951ba30f6f2370a2fcf415a";
+var s = document.getElementsByTagName("script")[0]; 
+s.parentNode.insertBefore(hm, s);
+})();
